92f9e9f33d9f466b6c9019460e07e48615126eda
Pavel <you@example.com>
2024-11-09 15:13:16 +0300

    feat: implement hint msg window

--- src/components/cardSubBanner/cardSubBanner.ts
+++ src/components/cardSubBanner/cardSubBanner.ts
@@ -25,7 +25,15 @@ export default class CardSubBanner {
             'https://raw.githubusercontent.com/PaHaNchickT/mvp-tariffs-banner/refs/heads/develop/src/public/icons/hint-icon.svg';
         hint.className = 'card_sub-banner__hint';
 
-        subBanner.append(cardImg, title, hint);
+        const hintMsg = document.createElement('div');
+        hintMsg.className = 'hint-msg';
+        hintMsg.textContent =
+            'Для Вашего спокойствия мы гарантируем возврат оплаты в случае отсутствия списания долгов после процедуры.';
+
+        hint.onmouseover = () => hintMsg.classList.toggle('hint-msg-vis');
+        hint.onmouseleave = () => hintMsg.classList.toggle('hint-msg-vis');
+
+        subBanner.append(cardImg, title, hint, hintMsg);
 
         return subBanner;
     }
